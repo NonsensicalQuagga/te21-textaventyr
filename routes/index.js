9702d1eef5e1a769c855544c4772758d4aa30dbc
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2024-01-23 11:15:24 +0100

    database pages

--- routes/index.js
+++ routes/index.js
@@ -34,4 +34,18 @@ router.get('/418', function (req, res) {
   // res.status(418)
 })
 
+const pool = require('../db')
+
+router.get('/dbtest/:id', async (req, res) =>{
+  try {
+    const id = req.params.id
+    const [parts] = await pool.promise().query(`SELECT * FROM alvin_part WHERE id = ${id}`)
+    const [options] =  await pool.promise().query(`SELECT * FROM alvin_option WHERE part_id = ${id}`)
+    res.json({parts, options})
+  } catch(error){
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
 module.exports = router
